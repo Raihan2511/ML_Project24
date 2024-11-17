17dec0047b191c64d0ce82584c793cfc334939ce
Raihan <uraihan2511@gmail.com>
2024-11-18 01:50:44 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -10,11 +10,13 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 
-    class CustomException(Exception):
-        def __init__(self,error_meassage,error_detail:sys):
-            super().__init__(error_message)
-            self.error_message=error_message_detail(error_message,error_detail=error_detail)
+class CustomException(Exception):
+    def __init__(self,error_meassage,error_detail:sys):
+        super().__init__(error_meassage)
+        self.error_message=error_message_detail(error_meassage,error_detail=error_detail)
 
 
-            def __str__(self):
-                return self.error_message
+        def __str__(self):
+            return self.error_message
+            
+
